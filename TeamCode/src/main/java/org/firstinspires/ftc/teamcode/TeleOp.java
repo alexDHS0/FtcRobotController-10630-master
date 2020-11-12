32a2732f95828d3261428f247a9c9b9bd72a391e
alexDHS0 <241franka@student.dist113.org>
2020-11-12 12:49:18 -0600

    improved teleOp and added functinoality to the arm motor, still need to do outake functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Disabled
 public class TeleOp extends LinearOpMode {
 
     DcMotor frontRight;
@@ -14,7 +15,10 @@ public class TeleOp extends LinearOpMode {
     Servo woblearm;
     boolean previousRb = false;
     boolean previousLb = false;
-
+    boolean previousA = false;
+    boolean previousB = false;
+    boolean previousCross = false;
+    boolean armManual = true;
     @Override
     //remember to comment you slop.
     public void runOpMode() throws InterruptedException {
@@ -27,36 +31,49 @@ public class TeleOp extends LinearOpMode {
 
         //do what we want to do(where stuff goes wrong)
         while (opModeIsActive()) {
-          driveset();
-          //grabset();
-          //armmove.setTargetPosition(1);
+            driveset();
+            grabset();
+
+            setmode();
+            //this is mostly so we have a working prototype which lets us use the
+            if (armManual == true) {
+            armcontroll();
+            }
+            else {
+            testarmadd();
+            testarmsub();
+            grabmove();
+            }
+            //armmove.setTargetPosition(1);
+            //grabmove();
 
         }
 
 
     }
-    /*
-    private void  grabmove(){
-        boolean lb = gamepad1.left_bumper;
 
-        if(lb&& !previousLb){
-            if(armmove.getTargetPosition() == 1){//1 is nit the right value
-                armmove.setTargetPosition(0);//1 is nit the right value
-            }
-            else{
-                armmove.setTargetPosition(90);//1 is nit the right value
-            }
-        }
-        previousLb = lb;
+    private void driveset() {
+        double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y); //we might have to take out the negatives.
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+
+        final double v1 = r * Math.cos(robotAngle) + rightX;//we may not need the final.
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        frontLeft.setPower(v1*1.41);//multiply them all by 1.414
+        frontRight.setPower(v2*1.41);
+        backLeft.setPower(v3*1.41);
+        backRight.setPower(v4*1.41);
     }
-    */
-    /*
+
     private void grabset(){
         boolean rb = gamepad1.right_bumper;
 
 
-        if(rb&& !previousRb){
-            if(woblearm.getPosition() == 1){
+        if(rb && !previousRb){
+            if(woblearm.getPosition() == 1.0){
                 woblearm.setPosition(0.0);
             }
             else{
@@ -65,22 +82,46 @@ public class TeleOp extends LinearOpMode {
         }
         previousRb = rb;
     }
-    */
-    private void driveset(){
-        double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y); //we might have to take out the negatives.
-        double robotAngle = Math.atan2(-gamepad1.left_stick_y,gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
 
-        final double v1 = r * Math.cos(robotAngle) + rightX;//we may not need the final.
-        final double v2 = r * Math.sin(robotAngle) - rightX;
-        final double v3 = r * Math.sin(robotAngle) + rightX;
-        final double v4 = r * Math.cos(robotAngle) - rightX;
+    private void  grabmove(){
+        boolean lb = gamepad1.left_bumper;
 
-        frontLeft.setPower(v1);//multiply them all by 1.414
-        frontRight.setPower(v2);
-        backLeft.setPower(v3);
-        backRight.setPower(v4);
+        if(lb && !previousLb){
+            if(armmove.getTargetPosition() == 90){//90 is probably not the bast value.
+                armmove.setTargetPosition(0);//0 is probably not the right value, in order to use 0 we would have to hold it in the same position we init at.
+            }
+            else{
+                armmove.setTargetPosition(90);//90 is probably not the best value.
+            }
+        }
+        previousLb = lb;
     }
+    //swich between manual forward and back and less well tested systems using set target position.
+    private void setmode (){
+        if(gamepad1.cross && !previousCross){
+            armManual=!armManual;
+            if(armManual){
+                armmove.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+            else {
+                armmove.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+        }
+    }
+    //manual contoll of the arm.
+    private void armcontroll(){
+        if(gamepad1.a) {
+            armmove.setPower(6.0);
+        }
+        else if(gamepad1.b){
+            armmove.setPower(-6.0);
+        }
+        else {
+            armmove.setPower(0.0);
+
+        }
+    }
+
     /*
     private void moveForeward() {
         //find the value the gamepad joystick reads.
@@ -92,24 +133,51 @@ public class TeleOp extends LinearOpMode {
         backRight.setPower(power);
     }
     */ //we dont need this anymore
-    public void initialize(){
-        //do the init stuff
+    public void initialize() {
 
-        woblearm =hardwareMap.get(Servo.class,"wa");
+        //initialize servos
+        woblearm = hardwareMap.get(Servo.class, "wa");
         woblearm.setPosition(1);//all we need to ever use.
 
-        //armmove = hardwareMap.get(DcMotor.class,"wm");
-        //armmove.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //armmove.setTargetPosition(1);
-        frontRight = hardwareMap.get(DcMotor.class,"fr");
-        frontLeft = hardwareMap.get(DcMotor.class,"fl");
-        backRight = hardwareMap.get(DcMotor.class,"br");
-        backLeft = hardwareMap.get(DcMotor.class,"bl");
+
+        //initialize drive train motors,
+        frontRight = hardwareMap.get(DcMotor.class, "fr");
+        frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        backRight = hardwareMap.get(DcMotor.class, "br");
+        backLeft = hardwareMap.get(DcMotor.class, "bl");
 
         frontRight.setPower(0);
         frontLeft.setPower(0);
         backLeft.setPower(0);
         backRight.setPower(0);
 
+        //initialize the weird wobble arm motor.
+        armmove = hardwareMap.get(DcMotor.class, "wm");
+        armmove.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //we set it so that the position the motor is in does not go any farther back when we reset it, and so that the motor hopefully does less work holding up the goal.
+        armmove.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //we make sure that the position is set to an acurate position we know.
+        armmove.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //let the motor use run to position.
+        //armmove.setTargetPosition(1);//ONE WILL PROBABLY BRAKE SOMETHING, FIND THE WORKING VALUES AND CHANGE THIS!
+        //armmove.setTargetPosition(10);
     }
+    //a manual test program that lets you manualy use set trget mode. split into a forward and backwards based on two buttons.
+    private void testarmadd(){
+        boolean Apress = gamepad1.a;
+        if(Apress && ! previousA){
+            int next = armmove.getTargetPosition() + 10;
+            armmove.setTargetPosition(next);
+        }
+        previousA = Apress;
+    }
+    private void testarmsub(){
+        boolean Bpress = gamepad1.b;
+        if(Bpress && ! previousB){
+            int next = armmove.getTargetPosition() + 10;
+            armmove.setTargetPosition(next);
+        }
+        previousB = Bpress;
+    }
+
 }
